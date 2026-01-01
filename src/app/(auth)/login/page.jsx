905a2ac7a25dcf2d1f974bfd0adfd7fc8fd7fc54
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2026-01-01 07:45:56 +0530

    commit

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Login page</div>
+    <div className='h-full flex flex-col items-center justify-center gap-15'>
+      <h1 className='text-white font-semibold text-3xl'>Log In</h1>
+      <div className='w-full flex flex-col items-center gap-9'>
+        <div className='flex flex-col w-full items-center gap-2'>
+          <label className='text-white text-sm self-start mx-15'>Phone</label>
+          <input type="text" className='bg-primaryDark outline-none h-12 w-[85%] rounded-lg text-white px-4' placeholder='Enter Phone'/>
+        </div>
+        <button className='text-black bg-white w-[85%] h-12 rounded-lg text-sm'>Continue</button>
+
+      </div>
+    </div>
   )
 }
 
--- src/app/(auth)/welcome-name/page.jsx
+++ src/app/(auth)/welcome-name/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div> welcome page</div>
-  )
-}
+    <div className="h-full flex flex-col items-center justify-center gap-15">
+      <h1 className="text-white font-semibold text-3xl">Welcome, You are?</h1>
+      <div className="w-full flex flex-col items-center gap-9">
+        <div className="flex flex-col w-full items-center gap-2">
+          <label className="text-white text-sm self-start mx-15">Name</label>
+          <input
+            type="text"
+            className="bg-primaryDark outline-none h-12 w-[85%] rounded-lg text-white px-4"
+            placeholder="Eg: john Mathe"
+          />
+        </div>
+        <button className="text-black bg-white w-[85%] h-12 rounded-lg text-sm">Continue</button>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
