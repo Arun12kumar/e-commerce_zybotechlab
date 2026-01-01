bac2e5c130080c55426ee22b03d2aaa0cb063ea7
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2026-01-01 17:52:13 +0530

    commit

--- src/app/(root)/myorders/page.jsx
+++ src/app/(root)/myorders/page.jsx
@@ -1,8 +1,15 @@
+import OrderCard from '@/components/OrderCard'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-[80vh]'>my orders page</div>
+    <div className='h-screen'>
+      <div className=' py-8 lg:py-17 space-y-6'>
+        <h1 className="text-4xl font-bold text-white mb-10">My Orders</h1>
+        <OrderCard/>
+        <OrderCard/>
+      </div>
+    </div>
   )
 }
 
